3c6baa6202d8e485d82879e646cff58b818f71b3
Ramdhan Rizki <Ramdhan Rizki>
2016-08-16 14:45:28 +0700

    Initial Commit

---
+++ search.js
@@ -0,0 +1,21 @@
+var Twitter = require('twitter');
+var express	= require('express');
+var app 	= express();
+
+var client = new Twitter({
+	consumer_key : '2Hmx6UXuxlRn7TicxrbTy1H2Q',
+	consumer_secret:'qgBVhwADPDKpljlTeS6HMF14PV64DNDNfvJykHvhDLMV5vutdf',
+	access_token_key:'126856000-00PtloL3L0uSZtlVyHOyUcjG4vQi5oohXfpImHlh',
+	access_token_secret:'KD5frjkG43GuSCvRVmDBycAxEXG5Bfr88KeH5GHDtEsSd'
+});
+app.get('/search',function(req,res){
+	client.get('search/tweets',{q:'HIV/AIDS'},function(error,tweets,response){
+		var data = JSON.parse(tweets); 
+		//console.log(data.);
+		res.json(tweets);
+	});	
+});
+
+app.listen(3000,function(err){
+	console.log('Listining on : 3000');
+});
